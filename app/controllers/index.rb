c4ddbe5ad05266c7c010da7159870f5d0157aaf8
Apprentice <apprentice@dbc-chicago034.local>
2013-10-30 10:40:42 -0500

    hope this works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,9 +7,30 @@ end
 post '/rolls' do
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
+  puts params
   value = params[:value] ? params[:value].to_i : nil
+
+
+  puts '##########'
+  puts value
+  puts '##########'
 
   @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  case value
+  when 1
+    erb :_one, :layout => false
+  when 2
+    erb :two, :layout => false
+  when 3
+    erb :_three, :layout => false
+  when 4
+    erb :_four, :layout => false
+  when 5
+    erb :_five, :layout => false
+  when 6
+    erb :_six, :layout => false
+  end
+
+  #erb :index  # HINT: what does this do? what should we do instead?
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+  $("form").on("submit", function( event ){
 
+
+    event.preventDefault();
+    var roll = (Math.floor(Math.random() * 6) + 1);
+    var rollJson = { "value": roll };
+    console.log( roll );
+    console.log( rollJson );
+
+    $.post("/rolls", rollJson, function( response ){
+      $( "#die" ).empty().append( response );
+    });
+
+
+
+
+
+    // post
+
+    // edit DOM thing
+
+
+
+  })
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
@@ -8,3 +31,10 @@ $(document).ready(function () {
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
 });
+
+
+
+// function submitRoll(){
+//   $().post(url, data, editDOM( response ));
+// };
+// function editDOM();
